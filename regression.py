cdd73622c1aa28ff34306d33b4c8ca06f5195cb1
Sahil Natvarlal Gohil <38555487+sahilgohil@users.noreply.github.com>
2024-03-31 22:36:25 +1100

    Update regression.py
    added stochastic gradient plus extra data

--- regression.py
+++ regression.py
@@ -1,25 +1,59 @@
+import numpy as np
 # ###################################################
 # Modeling - What we want to compute
-points = [(2,4),(4,2)]
+# points = [(np.array([2]),4),(np.array([4]),2)]
+# d = 1
+true_w = np.array([1,2,3,4,5])
+d = len(true_w)
+
+points = []
+for i in range(0000):
+    x = np.random.rand(d)
+    y = true_w.dot(x) + np.random.rand()
+    points.append((x,y))
+
+
+
 
 def F(w):
-    result = sum((w * x - y) ** 2 for x,y in points)
+    result = sum((w.dot(x) - y) ** 2 for x,y in points)/len(points)
     return result
 def dF(w):
-    result = sum(2*(w * x-y)*x for x,y in points)
+    result = sum(2*(w.dot(x)-y)*x for x,y in points)/len(points)
+    return result
+
+def sF(w, i):
+    (x,y) = points[i]
+    result = (w.dot(x)-y)**2
+    return result
+def sdF(w, i):
+    (x,y) = points[i]
+    result = 2*(w.dot(x)-y)*x
     return result
 
 ######################################################
 # Algorithms - How we want to compute it
 def gradientDescent(F , dF, d):
-    w = 0
+    w = np.zeros(d) # initial guess
     eta = 0.01 # learning rate or step for each iteration
 
-    for i in range(100):
+    for i in range(1000):
         
         value = F(w)
         gradient = dF(w)
         w = w-eta*gradient
         print(f'iteration {i}: w = {w}, F(w) = {value}')
+def stocasticGradientDescent(sF, sdF, d, n):
+        w = np.zeros(d)
+        numOfUpdates = 0
 
-gradientDescent(F,dF,1)
+        for t in range(1000):
+            for i in range(n):
+                  value = sF(w,i)
+                  gradient = sdF(w,i)
+                  numOfUpdates+=1
+                  eta = 1/numOfUpdates
+                  w = w-eta*gradient
+            print(f'iteration {t}: w = {w}, F(w) = {value}')
+# gradientDescent(F,dF,d)
+stocasticGradientDescent(sF,sdF,d,len(points))
