557c7b57e44969f8a9933df46f2bff6203d72add
Sahil Natvarlal Gohil <38555487+sahilgohil@users.noreply.github.com>
2024-03-31 08:43:59 +0000

    gradient descent

---
+++ regression.py
@@ -0,0 +1,25 @@
+# ###################################################
+# Modeling - What we want to compute
+points = [(2,4),(4,2)]
+
+def F(w):
+    result = sum((w * x - y) ** 2 for x,y in points)
+    return result
+def dF(w):
+    result = sum(2*(w * x-y)*x for x,y in points)
+    return result
+
+######################################################
+# Algorithms - How we want to compute it
+def gradientDescent(F , dF, d):
+    w = 0
+    eta = 0.01 # learning rate or step for each iteration
+
+    for i in range(100):
+        
+        value = F(w)
+        gradient = dF(w)
+        w = w-eta*gradient
+        print(f'iteration {i}: w = {w}, F(w) = {value}')
+
+gradientDescent(F,dF,1)
