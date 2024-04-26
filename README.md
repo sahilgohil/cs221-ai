79a60965403184630d6d1187083236bc861b4459
Sahil Natvarlal Gohil <38555487+sahilgohil@users.noreply.github.com>
2024-04-26 10:11:37 +0000

    update readme with transportaion problem

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # cs221-ai
+
+# Search Problems
+Things required in a search problem,
+
+1. State
+2. Action
+3. isEndState(state) -> returns true or false
+4. Cost(state,action) -> returns the cost for the action on this state
+5. succ(state,action) -> return the successor state using the action on this state
+
+# Model Transportation problem
+- end state
+- contructor
+- start state function -> returns the initial state
+- isEnd function
+- succandcost function -> returns the list of [action, newstate, costoftheaction]
+# Backtracing Algorithm for search problem
+
--- regression.py
+++ regression.py
@@ -1,18 +1,19 @@
+import numpy as np
 # ###################################################
 # Modeling - What we want to compute
-points = [(2,4),(4,2)]
-
+points = [(np.array([2]),4),(np.array([4]),2)]
+d = 1
 def F(w):
-    result = sum((w * x - y) ** 2 for x,y in points)
+    result = sum((w.dot(x) - y) ** 2 for x,y in points)
     return result
 def dF(w):
-    result = sum(2*(w * x-y)*x for x,y in points)
+    result = sum(2*(w.dot(x)-y)*x for x,y in points)
     return result
 
 ######################################################
 # Algorithms - How we want to compute it
 def gradientDescent(F , dF, d):
-    w = 0
+    w = np.zeros(d) # initial guess
     eta = 0.01 # learning rate or step for each iteration
 
     for i in range(100):
@@ -22,4 +23,4 @@ def gradientDescent(F , dF, d):
         w = w-eta*gradient
         print(f'iteration {i}: w = {w}, F(w) = {value}')
 
-gradientDescent(F,dF,1)
+gradientDescent(F,dF,d)
