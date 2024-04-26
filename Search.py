699b1a1804ad0c74f7922e830c88577b2df0cbc0
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-04-26 10:37:11 +0000

    added transportation model class in seach.py

---
+++ Search.py
@@ -0,0 +1,20 @@
+# this is a Model class for the transportation problem
+class TransportationProblem(object):
+    def __init__(self, N):
+        # N = number of blocks
+        self.N = N
+    
+    def startState(self):
+        return 1
+    
+    def isEndState(self, state):
+        return state == self.N
+    
+    def getSuccAndCost(self, state):
+        result = []
+        if state + 1 <= self.N:
+            result.append('Walk',(state + 1), 1)
+        if state * 2 <= self.N:
+            result.append('Tram',(state*2), 2)
+        return result
+    
