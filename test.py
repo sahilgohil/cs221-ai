d56b7967c5c9d5c8b41c1d89d2ab1c1cd98faabb
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-04-27 02:40:44 +0000

    added test case for dfs algo

--- test.py
+++ test.py
@@ -8,6 +8,12 @@ class TestBacktracking(unittest.TestCase):
         problem = Search.TransportationProblem(10)
         minimumCost = Search.backtracking(problem)
         self.assertEqual(minimumCost, 6)
+    def test_dfs(self):
+        # test case for dfs algorithm
+        problem = Search.TransportationProblem(10)
+        solution = Search.dfs(problem)
+        expectedSolution = ['Tram', 'Tram', 'Tram', 'Walk', 'Walk']
+        self.assertEqual(solution, expectedSolution)
 
 if __name__ == '__main__':
     unittest.main()
